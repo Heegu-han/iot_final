fda48a2a287a2e09e752744a1595664f98799c8c
Heegu-han <hanheegu7@gmail.com>
2021-06-12 03:45:17 +0000

    aws

---
+++ sensor_data_collector.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import request
+import time
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Temperature and Humidity Data Collector</h1>"
+
+@app.route("/temp_hum")
+def collect_temp_hum():
+    temp = request.args.get("temp")
+    hum = request.args.get("hum")
+
+    with open("temp_hum.csv","a",encoding="utf-8") as fp:
+        fp.write(f"{time.time()},{temp},{hum}\n")
+
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
